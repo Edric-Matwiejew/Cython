9e1c0fe8dc13ec5cb4bcfee1e8c083864e01a1cb
Edric Matwiejew <43461963+Edric-Matwiejew@users.noreply.github.com>
2021-04-29 16:16:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import PyQuantumWalk
-import CyQuantumWalk
 import matplotlib.pyplot as plt
 import time
 
@@ -12,11 +11,6 @@ start = time.time()
 nodeProbabilities = PyQuantumWalk.QuantumWalk(numberOfNodes, startingNode, startingSpin, numberOfSteps)
 ptime = time.time() - start
 print('Time: ', time.time() - start)
-
-start = time.time()
-nodeProbabilities = CyQuantumWalk.QuantumWalk(numberOfNodes, startingNode, startingSpin, numberOfSteps)
-print('Time: ', ptime/(time.time() - start))
-
 
 plt.plot(nodeProbabilities)
 plt.savefig('quantumWalk')
