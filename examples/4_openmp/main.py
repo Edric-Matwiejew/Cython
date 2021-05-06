16cb290a7f7405b9a943cac67d4f294c924c1613
Edric-Matwiejew <edric.matviev@gmail.com>
2021-05-06 15:03:33 +0800

    OpenMP examples.

---
+++ examples/4_openmp/main.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.insert(1,'compiled_modules/')
+
+import parallel_block
+import parallel_loop_addition
+import parallel_loop_reduction
---
+++ examples/4_openmp/parallel_loop_reduction.pyx
@@ -0,0 +1,26 @@
+
+from cython.parallel import prange
+
+
+cdef:
+    int i, n = 100000
+    int a[100000]
+    int b[100000]
+    int c[100000]
+    int total
+
+for i in prange(n, nogil = True):
+    a[i] = i
+    b[i] = i
+
+for i in prange(n, nogil = True):
+    total += a[i] + b[i]
+
+print(<double>total/n)
+
+
+
+
+
+
+
